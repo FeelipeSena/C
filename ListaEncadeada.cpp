d66b57afd76b3a580edfed3d73d827316e107b87
Felipe Sena <feelipe.27@gmail.com>
2019-03-11 16:04:19 -0300

    update

---
+++ ListaEncadeada.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct lista {
+	int info;
+	struct lista*prox;
+};
+typedef struct lista Lista;
+Lista* insere (Lista* l, int);
+Lista* inicializa (void);
+void imprime (Lista* l);
+
+int main (void){
+Lista* l; /* declara uma lista não inicializada */
+l = inicializa(); /* inicializa lista como vazia */
+l = insere(l, 23); /* insere na lista o elemento 23 */
+l = insere(l, 45); /* insere na lista o elemento 45 */
+imprime(l);
+
+return 0;
+}
+
+
+/* inserção no início: retorna a lista atualizada */
+Lista* insere (Lista* l, int i){
+	Lista* novo = (Lista*) malloc(sizeof(Lista));
+	novo->info = i;
+	novo->prox = l;
+	return novo;
+}
+
+/* função de inicialização: retorna uma lista vazia */
+Lista* inicializa (void){
+	return NULL;
+}
+
+/* função imprime: imprime valores dos elementos */
+void imprime (Lista* l){
+	Lista* p; /* variável auxiliar para percorrer a lista */
+	for (p = l; p != NULL; p = p->prox)
+	printf("info = %d\n", p->info);
+}
---
+++ ListasExercises.c
@@ -0,0 +1,166 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+struct lista {
+	int info;
+	struct lista* prox;
+};
+
+typedef struct lista Lista;
+
+
+Lista* inicializa(){
+	return NULL;
+}
+
+Lista* insere (Lista* l, int n){
+	Lista* novo = (Lista*) malloc(sizeof(Lista));
+	novo->info = n;
+	novo->prox = l;
+	
+	return novo;
+}
+
+void imprime(Lista* l){
+	Lista* q;	
+	
+	do{
+		printf(" %i ", l->info);
+		l = l->prox;
+	} while(l != NULL);
+}
+
+int comprimento(Lista* l){
+	int i;
+	Lista* p = l;
+	
+	for(i = 0; p != NULL; p = p->prox, i++);
+	
+	return i;
+}
+
+int maiorQueN(Lista* l, int n){
+	Lista* p = l;
+	int i = 0;
+	
+	while(p != NULL){
+		if(p->info > n)
+			i++;
+			
+		p = p->prox;					
+	}	
+	return i;
+}
+
+Lista* substitui(Lista* l, int old, int n){
+	Lista* p = l;
+	
+	while(p != NULL){
+		if(p->info == old)
+			p->info = n;
+			
+		p = p->prox;
+	}
+	return l;
+}
+int pos = 0;
+
+Lista* ultimo_x(Lista* l, int x){
+	Lista* p = l;
+	Lista* ultimo;
+	
+	while(p != NULL){
+		if(p->info == x){
+			ultimo = p;
+		}
+		p = p->prox;
+		pos++;
+	}
+	
+	return ultimo;
+}
+
+Lista* concatena(Lista* l1, Lista* l2){
+	Lista* final = l1;
+	
+	for( ;final->prox != NULL; final = final->prox);
+	
+	final->prox = l2;
+	
+	return l1;
+}
+
+Lista* retira(Lista* l, int n){
+	Lista* p = l;
+	Lista* anterior = NULL;
+	Lista* aux;
+	Lista* removable;
+	int cont = 0;
+	
+	while(p != NULL){
+		if(p->info == n){
+			aux = p->prox;
+			removable = p;
+			
+			if(anterior != NULL)	{
+				p = anterior;
+				p->prox = aux;
+				free(removable);
+			} else {			
+				p = p->prox;
+				l = p;					
+				free(removable);				
+			}
+			
+		}		
+		anterior = p;
+		p = p->prox;
+	}
+	
+	return l;
+}
+
+Lista* separa(Lista* l, int n){
+	Lista* p  = l;
+	Lista* l2 = NULL;
+	
+	while(p != NULL){
+		if(p->info == n){
+			l2 = p->prox;
+			p->prox = NULL;
+			return l2;
+		}
+		p = p->prox;
+	}
+	return l2;
+}
+
+int main(){
+	Lista* lista = inicializa();
+	Lista* ultimo;
+	Lista* segunda = NULL;
+	
+	Lista* lista2 = inicializa();
+	
+	int i = 0;
+	int j = 10;
+	
+	for(i ; i < 10; i++){
+		lista = insere(lista, i);
+	}
+	
+	for(j; j < 20; j++){
+		lista2 = insere(lista2, j);
+	}
+	
+	lista = concatena(lista, lista2);		
+	
+	lista = retira(lista, 19);
+
+	segunda = separa(lista, 10);
+	printf("Lista1: ");
+	imprime(lista);	
+	
+	printf("\nLista2: ");
+	imprime(segunda);
+}
