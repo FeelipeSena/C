a949b461c2a8674d1546b8cb22aca8cd245407bb
Felipe Sena <feelipe.27@gmail.com>
2019-04-04 14:01:15 -0300

    up

---
+++ Pilhas/pilhas.h
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+//NÓ DA LISTA PARA ARMAZENAR VALORES
+typedef struct no {
+    float info;
+    struct no *prox;
+}No;
+ 
+//ESTRUTURA DA PILHA
+typedef struct pilha {
+    No *prim;
+}Pilha;
+ 
+// PROTOTIPOS
+Pilha *cria(void);
+No *ins_ini (No *l, float v);
+No *ret_ini (No *l);
+void push (Pilha *p, float v);
+float pop (Pilha *p);
+int vazia (Pilha *p);
+void libera (Pilha *p);
+void imprime (Pilha *p);
+ 
+//INICIA A LISTA COMO SENDO VAZIA
+Pilha *cria(void) {
+    Pilha *p = (Pilha*)malloc(sizeof(Pilha));
+    p->prim = NULL;
+    return p;
+}
+ 
+//FUNÇÃO AUXILIAR: INSERE NO INICIO
+No *ins_ini (No *l, float v){
+    No *p = (No*)malloc(sizeof(No));
+    p->info = v;
+    p->prox = l;
+    return p;
+}
+ 
+//FUNÇÃO AUXILIAR: RETIRA DO INÍCIO
+No *ret_ini (No *l){
+    No *p = l->prox;
+    free(l);
+    return p;
+}
+ 
+//FUNÇÃO COLOCA NA PILHA
+void push (Pilha *p, float v){
+    p->prim = ins_ini(p->prim, v);
+}
+ 
+//FUNÇÃO RETIRA DA LISTA
+float pop (Pilha *p){
+    float v;
+    if (vazia(p)) {
+        printf ("PILHA VAZIA!\n");
+        exit (1);
+    }
+    v = p->prim->info;
+    p->prim = ret_ini(p->prim);
+    return v;
+}
+ 
+//A PILHA ESTARÁ VAZIA SE A LISTA ESTIVER VAZIA
+int vazia (Pilha *p){
+    return (p->prim == NULL);
+}
+ 
+//LIBERA TODOS OS ELEMENTOS DA LISTA E DEPOIS LIBERA A PILHA
+void libera (Pilha *p) {
+    No *q = p->prim;
+    while (q!=NULL) {
+        No *t = q->prox;
+        free(q);
+        q = t;
+    }
+    free(p);
+}
+ 
+//IMPRIME A PILHA
+void imprime (Pilha *p){
+    No *q;
+    for (q=p->prim; q!=NULL; q=q->prox){
+        printf ("%.2f\n",q->info);
+    }
+}
---
+++ Pilhas/pilhasTeste.cpp
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pilhas.h"
+ 
+Pilha *concat(Pilha *p, Pilha *q){
+    Pilha *aux = cria();
+    float v;
+    push (aux, v=pop(q));
+    push (aux, v=pop(q));
+    push (aux, v=pop(q));
+    push (aux, v=pop(q));
+    push (p, pop(aux));
+ 
+    push (p, pop(aux));
+ 
+    push (p, pop(aux));
+ 
+    push (p, pop(aux));
+    return p;
+ 
+}
+ 
+int main(){
+    Pilha *p = cria();
+    Pilha *q = cria();
+    float v;
+    push(p, 1);
+    push(p, 2);
+    push(p, 3);
+    push(p, 4);
+    
+
+    push(q, 5);
+    push(q, 6);
+    push(q, 7);
+    push(q, 8);
+ 
+    imprime (p);
+    printf ("\n");
+    imprime (q);
+	
+    p = concat(p, q);
+ 
+    printf ("\n");
+    imprime(p);
+ 
+    return 0;
+}
